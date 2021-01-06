4d0cab1092af2b489ea0d3a2d0d4a1f64cf4bd06
Adrian Storr <Adrianloves123@gmail.com>
2021-01-06 14:08:13 -0500

    created database.js, mainifest.json, and service worker.js files

--- public/database.js
+++ public/database.js
@@ -24,3 +24,38 @@ request.onsuccess = ({ target }) => {
 request.onerror = function(event) {
   console.log("Woops! " + event.target.errorCode);
 };
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"],"readwrite");
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(["pending"],"readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => {        
+        return response.json();
+      })
+      .then(() => {
+        // delete records if successful
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+      });
+    }
+  };
+}
